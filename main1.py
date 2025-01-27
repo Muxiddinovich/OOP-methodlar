b9fcb8cb8c81a2cb8b8a8725363689bf519b5c62
Sherbek <sherbekkubayev@gmail.com>
2025-01-27 13:52:01 +0500

    OOP Metodlari

---
+++ main1.py
@@ -0,0 +1,45 @@
+# class Pupil:
+#     school_name="ABC"
+
+#     def __init__(self, name):
+#         self.name=name
+
+#     @classmethod
+#     def change_name(cls, name):
+#         Pupil.school_name=name
+
+
+# pupil=Pupil("Sherbek")
+
+# print(pupil.school_name)
+
+# Pupil.change_name("PDP School")
+
+# print(pupil.school_name)
+
+
+# from typing import Any
+
+
+# class Talaba:
+#     summa1=30000000
+
+
+#     def __init__(self, summa):
+#         self.summa=summa
+
+
+#     @classmethod
+#     def change_summa(cls, summa):
+#         Talaba.change_summa=summa
+
+
+# talaba=Talaba("36000000")
+# print(talaba.summa1)
+# Talaba.change_summa(36000000)
+# print(talaba.summa)
+
+
+
+
+
